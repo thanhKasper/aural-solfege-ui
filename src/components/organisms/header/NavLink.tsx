d088012ca432d611a9f49d7be744053393c4ae1b
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-05-06 00:31:18 +0700

    feat: complete the navigation item

--- src/components/organisms/header/NavLink.tsx
+++ src/components/organisms/header/NavLink.tsx
@@ -14,5 +14,21 @@ export const NavLink = styled(Link)<NavLinkProps>(({ theme, isActive }) => ({
   paddingRight: 16,
   cursor: "pointer",
   backgroundColor: isActive ? theme.palette.primary.main : "inherit",
-  ":hover": {},
+  position: "relative",
+  ...(!isActive && {
+    ":hover": {
+      backgroundColor: theme.palette.secondary.dark,
+    },
+  }),
+  "&::after": isActive
+    ? {
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        content: "''",
+        width: "100%",
+        height: 4,
+        backgroundColor: theme.palette.surface.main,
+      }
+    : {},
 }));
--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,15 @@
 import { createTheme } from "@mui/material";
 
+declare module "@mui/material/styles" {
+  interface Palette {
+    surface: Palette["primary"];
+  }
+
+  interface PaletteOptions {
+    surface?: PaletteOptions["primary"];
+  }
+}
+
 const theme = createTheme({
   colorSchemes: {
     light: {
@@ -9,8 +19,14 @@ const theme = createTheme({
         },
         secondary: {
           main: "#9ECAD6",
+          light: "#C8E6EE",
+          dark: "#87BECD",
         },
-        
+        surface: {
+          main: "#FFEAEA",
+          400: "#FFF5F5",
+          300: "#FFFAFA",
+        },
       },
     },
   },
