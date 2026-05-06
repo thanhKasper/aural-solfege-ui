287568d4adf9a1d103a5faacedf3e50e4aa3d4ab
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-05-06 23:32:31 +0700

    feat: reset CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./reset.css";
 import theme from "./theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <App />
     </ThemeProvider>
   </StrictMode>,
