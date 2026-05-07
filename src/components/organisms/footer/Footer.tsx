e5782805d009e70e9f9cfecad81e2d2022c5f0c4
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-05-07 23:20:12 +0700

    feat: create footer

--- src/components/organisms/footer/Footer.tsx
+++ src/components/organisms/footer/Footer.tsx
@@ -1,3 +1,96 @@
+import LogoWithBrandName from "@/components/molecules/LogoWithBrandName";
+import {
+  Box,
+  Button,
+  Container,
+  MenuItem,
+  Paper,
+  Select,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const FooterContainer = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.secondary.main,
+}));
+
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <FooterContainer
+      sx={{ paddingY: 8, display: "flex", justifyContent: "space-between", paddingX: 16 }}
+    >
+      {/* Left: Logo + Language selector */}
+      <Stack spacing={4}>
+        <LogoWithBrandName size="xl" />
+
+        {/* Language selector */}
+        <Select defaultValue="en" size="small" sx={{ minWidth: 200 }}>
+          <MenuItem value="en">English</MenuItem>
+          <MenuItem value="fr">Français</MenuItem>
+          <MenuItem value="es">Español</MenuItem>
+        </Select>
+      </Stack>
+
+      {/* Center: Nav link columns */}
+      <Stack direction={"row"} component="nav" spacing={4}>
+        {/* Other sites */}
+        <Stack direction={"column"}>
+          <Typography variant="subtitle1">Other sites</Typography>
+          <Typography component="a" href="#">
+            Sound configuration
+          </Typography>
+          <Typography component="a" href="#">
+            Ear training exercises
+          </Typography>
+        </Stack>
+
+        {/* Services */}
+        <Stack direction={"column"}>
+          <Typography variant="subtitle1">Services</Typography>
+          <Typography component="a" href="#">
+            Ear training system
+          </Typography>
+          <Typography component="a" href="#">
+            Hear together
+          </Typography>
+        </Stack>
+
+        {/* About */}
+        <Stack direction={"column"}>
+          <Typography variant="subtitle1">About</Typography>
+          <Typography component="a" href="#">
+            About me
+          </Typography>
+          <Typography component="a" href="#">
+            About this page
+          </Typography>
+        </Stack>
+      </Stack>
+
+      {/* Right: Feedback form */}
+      <Stack direction={"column"} spacing={4}>
+        <Typography variant="subtitle1">Your opinion matters!</Typography>
+
+        <TextField
+          type="email"
+          placeholder="Enter your email"
+          size="small"
+          fullWidth
+        />
+
+        <TextField
+          placeholder="Anything you want to be improved..."
+          multiline
+          rows={6}
+          fullWidth
+        />
+
+        <Button variant="contained" sx={{ alignSelf: "flex-start" }}>
+          A button
+        </Button>
+      </Stack>
+    </FooterContainer>
+  );
 };
