3b89c3a8d8981b99b5216a348bd6593976c92f5e
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-05-08 23:57:39 +0700

    feat: create exercise page

---
+++ src/pages/ExercisesPage.tsx
@@ -0,0 +1,13 @@
+import { Container, Input } from "@mui/material";
+
+const ExercisesPage = () => {
+  return (
+    <Container>
+      <Input size="medium" placeholder="search..." fullWidth />
+      <h1>This is a header</h1>
+      ExercisesPage
+    </Container>
+  );
+};
+
+export default ExercisesPage;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import Layout from "@/layouts/Layout";
+import ExercisesPage from "@/pages/ExercisesPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    children: [
+      {
+        path: "/exercises",
+        element: <ExercisesPage />,
+      },
+    ],
   },
 ]);
 
