122a26d73cada404443e77f9d0f33f2e8cb41436
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-05-09 10:23:55 +0700

    feat: create overview page

---
+++ src/pages/OverviewPage.tsx
@@ -0,0 +1,7 @@
+import { Container } from "@mui/material";
+
+const OverviewPage = () => {
+  return <Container>OverviewPage</Container>;
+};
+
+export default OverviewPage;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import Layout from "@/layouts/Layout";
 import ExercisesPage from "@/pages/ExercisesPage";
+import OverviewPage from "@/pages/OverviewPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
+      {
+        index: true,
+        element: <OverviewPage />,
+      },
       {
         path: "/exercises",
         element: <ExercisesPage />,
