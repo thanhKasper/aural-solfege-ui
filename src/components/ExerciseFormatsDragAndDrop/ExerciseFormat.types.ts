2f5e92e5f405b53420a19d70063097d1347d6766
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-05-30 00:37:58 +0700

    feat: add TExerciseFormat and IExerciseFormatSourceElement to shared types

--- src/components/ExerciseFormatsDragAndDrop/ExerciseFormat.types.ts
+++ src/components/ExerciseFormatsDragAndDrop/ExerciseFormat.types.ts
@@ -1,4 +1,14 @@
+import type { TIntervalTrainingExercise } from "./IntervalEarTraining";
+
 export type TBaseExerciseFormat<T> = {
   id: string;
   position: number; // zero-based index
 } & T;
+
+export type TExerciseFormat = TIntervalTrainingExercise;
+
+export interface IExerciseFormatSourceElement {
+  onCreated: (data: TExerciseFormat) => void;
+  onChanged: (data: TExerciseFormat) => void;
+  onRemoved: (data: TExerciseFormat) => void;
+}
