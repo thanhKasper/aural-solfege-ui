1612f5eaf5abdc21df67b9f50ed59e6df4d6cab2
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-07-05 16:51:29 +0700

    feat: add quarter-rest and infinite icons

--- src/components/atoms/Icon.tsx
+++ src/components/atoms/Icon.tsx
@@ -1,16 +1,22 @@
 import RestIcon from "@/assets/rest.svg";
+import QuarterRest from "@/assets/quarter-rest.svg";
+import InfiniteIcon from "@/assets/infinite.svg";
 import { SvgIcon, type SvgIconProps } from "@mui/material";
 import type { ElementType } from "react";
 
-const stringIconMapper: Record<string, ElementType> = {
+type IconName = "rest" | "quarter-rest" | "infinite";
+
+const stringIconMapper: Record<IconName, ElementType> = {
   rest: RestIcon as ElementType,
+  "quarter-rest": QuarterRest as ElementType,
+  infinite: InfiniteIcon as ElementType,
 };
 
 const Icon = ({
   icon,
   ...props
 }: {
-  icon: string;
+  icon: IconName;
   color?: SvgIconProps["color"];
 } & Omit<SvgIconProps, "component">) => {
   const Component = stringIconMapper[icon];
