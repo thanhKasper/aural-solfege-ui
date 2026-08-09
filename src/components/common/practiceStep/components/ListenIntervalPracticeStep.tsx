231c3a1cc652d7d66a3eb187b83e40c5d95502af
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-08-09 21:22:42 +0700

    feat: create specific step for Interval Listening activity

--- src/components/common/practiceStep/components/ListenIntervalPracticeStep.tsx
+++ src/components/common/practiceStep/components/ListenIntervalPracticeStep.tsx
@@ -1,8 +1,45 @@
 import type { PracticeStepResponse } from "@/providers/auralSolfege/apis.type";
-import type { FC } from "react";
+import { getIntervalSound } from "@/providers/musicAudio/apis";
+import { getIntervalNotation } from "@/utils/retrieveMusicalInterval";
+import { Button } from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useRef, type FC } from "react";
 
-const ListenIntervalPracticeStep: FC<PracticeStepResponse> = (props) => {
-  return <div>ListenIntervalPracticeStep ${JSON.stringify(props)}</div>;
+const ListenIntervalPracticeStep: FC<PracticeStepResponse> = ({
+  currentStep,
+}) => {
+  const { data } = useQuery({
+    queryKey: [
+      `${currentStep.interval}:${currentStep.texture}:${currentStep.direction}`,
+    ],
+    queryFn: async () =>
+      await getIntervalSound({
+        direction: currentStep.direction,
+        texture: currentStep.texture,
+        interval: getIntervalNotation(currentStep.interval),
+      }),
+  });
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  useEffect(() => {
+    if (!data) return;
+    audioRef.current?.pause();
+    audioRef.current = new Audio(URL.createObjectURL(data));
+    return () => {
+      audioRef.current?.pause();
+      if (audioRef.current?.src) URL.revokeObjectURL(audioRef.current.src);
+    };
+  }, [data]);
+
+  return (
+    <div>
+      You are listening to interval {currentStep.interval} -{" "}
+      {currentStep.direction} - {currentStep.texture}
+      <div>
+        <Button onClick={() => audioRef.current?.play()}>Play</Button>
+      </div>
+    </div>
+  );
 };
 
 export default ListenIntervalPracticeStep;
