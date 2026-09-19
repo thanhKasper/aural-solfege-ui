f87d5ebe9c1d0f9c9510ab0bfe51ae9b80ebbd24
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-09-19 09:50:51 +0700

    feat: dispatch event when source element is dropped

--- src/components/organisms/DragAndDrop/elements/Source.tsx
+++ src/components/organisms/DragAndDrop/elements/Source.tsx
@@ -1,14 +1,28 @@
-import { type PropsWithChildren } from "react";
+import { useCallback, useEffect, type PropsWithChildren } from "react";
 import { useDragAndDrop } from "../hooks/useDragAndDrop";
 import type { RelocatableContentRenderer } from "./types";
+import { useEventBus } from "@/hooks/useEventBus";
+import { DRAG_AND_DROP_EVENT } from "../constants";
 
 interface SourceProps extends PropsWithChildren {
   onBeforeRelocatableCreated?: () => void;
   renderRelocatableContent?: RelocatableContentRenderer;
 }
 
-const Source = ({ children }: SourceProps) => {
+const Source = ({ children, onBeforeRelocatableCreated }: SourceProps) => {
   const { showGhostComponent } = useDragAndDrop();
+  const { dispatch } = useEventBus();
+
+  const handleMouseRelease = useCallback(() => {
+    dispatch(DRAG_AND_DROP_EVENT.ELEMENT_DROP, { onBeforeRelocatableCreated });
+  }, [dispatch, onBeforeRelocatableCreated]);
+
+  useEffect(() => {
+    window.addEventListener("mouseup", handleMouseRelease);
+    return () => {
+      window.removeEventListener("mouseup", handleMouseRelease);
+    };
+  }, [handleMouseRelease]);
 
   return (
     <div onMouseDown={(e) => showGhostComponent(children, e.currentTarget)}>
