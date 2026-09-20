5a058206dad67415579311aedfd411f17e760d3a
Kiều Tiến Thành <kieutienthanh207@gmail.com>
2026-09-20 14:43:34 +0700

    feat: enhance drag and drop context to support adding element

--- src/components/organisms/DragAndDrop/providers/DragAndDropContextV2.tsx
+++ src/components/organisms/DragAndDrop/providers/DragAndDropContextV2.tsx
@@ -1,21 +1,17 @@
+import type { Container } from "@/services/dragAndDrop/Container";
 import {
   createContext,
   type Dispatch,
   type ReactNode,
+  type RefObject,
   type SetStateAction,
 } from "react";
 
 interface DragAndDropContextProps {
   setGhost: Dispatch<SetStateAction<ReactNode>>;
-  // showGhostComponent: (
-  //   element: HTMLElement,
-  //   onSuccessDrop?: () => void,
-  // ) => void;
-  // hideGhostComponent: () => void;
+  containersRef?: RefObject<Map<string, Container>>;
 }
 
 export const DragAndDropContext = createContext<DragAndDropContextProps>({
   setGhost: () => {},
-  // showGhostComponent: () => {},
-  // hideGhostComponent: () => {},
 });
